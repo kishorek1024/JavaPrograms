95f7a62295c21317708f3140b62fcf326ed64646
DELL <DELL@DESKTOP-BPC4G7D>
2019-07-15 08:41:01 +0530

    Release2

--- JavaProj/src/packtwo/StaticVar2.java
+++ JavaProj/src/packtwo/StaticVar2.java
@@ -1,12 +1,11 @@
 package packtwo;
 
-import packone.StaticVar;
 
 public class StaticVar2 {
 
 	public static void main(String[] args) {
 		
-		System.out.println(StaticVar.a*StaticVar.b);
+		System.out.println("Hi");
 
 	}
 
